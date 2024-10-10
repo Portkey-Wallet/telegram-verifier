a751b1160ba8730189733f852ceb3e010128b999
Edward-BXS <Magic.Wizard@portkey.finance>
2024-10-10 18:02:16 +0800

    framework upgrade

--- src/TelegramServer.Auth.HttpApi/Controllers/TelegramAuthController.cs
+++ src/TelegramServer.Auth.HttpApi/Controllers/TelegramAuthController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
+using Asp.Versioning;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
